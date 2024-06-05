7a7f77ec3b4320fbc145b814e598605fb7fef40e
JHBDVT\mmsomi2 <mmsomi2@jhb.dvt.co.za>
2024-06-05 17:22:25 +0200

    added handler

---
+++ shop-microservices-app/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,28 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                Image = command.Image,
+                Price = command.Price
+            };
+
+            // Save to database
+
+            // Will return the Id of the newly created product once saved to the database. Placeholder id for now.
+            return new CreateProductResult(Guid.NewGuid());
+        }
+    }
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string Image, decimal Price) : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+}
