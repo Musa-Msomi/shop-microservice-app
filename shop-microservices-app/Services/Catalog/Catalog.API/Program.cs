dbece547887a59405f7f4cd8fb47aa04a7712497
JHBDVT\mmsomi2 <mmsomi2@jhb.dvt.co.za>
2024-06-05 17:05:23 +0200

    registered Mediatr

--- shop-microservices-app/Services/Catalog/Catalog.API/Program.cs
+++ shop-microservices-app/Services/Catalog/Catalog.API/Program.cs
@@ -4,6 +4,11 @@ using Carter;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
+
 
 var app = builder.Build();
 app.MapCarter();
