41b7b08e609ff935babd860cfe2da142c5ec2768
JHBDVT\mmsomi2 <mmsomi2@jhb.dvt.co.za>
2024-06-19 12:06:05 +0200

    added Handler class for getting product by category

---
+++ shop-microservices-app/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Exceptions;
+using Catalog.API.Models;
+using Marten;
+
+namespace Catalog.API.Products.GetProductByCategory
+{
+    public record GetProductByCategoryQuery(string Category) : IQuery<GetProductByCategoryResult>;
+    public record GetProductByCategoryResult(IEnumerable<Product> Products);
+    internal class GetProductByCategoryQueryHandler(IDocumentSession session, ILogger<GetProductByCategoryQueryHandler> logger) :
+        IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+    {
+        public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery request, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Querying for products by category");
+            var products = await session.Query<Product>().Where(p => p.Category.Contains(request.Category)).ToListAsync(cancellationToken);
+            //if (!products.Any())
+            //{
+            //    throw new ProductCategoryNotFoundException();
+            //}
+            return new GetProductByCategoryResult(products);
+        }
+    }
+}
