c2a926b3eedf375cfe5a57d48c9cfcdf2a0099cd
JHBDVT\mmsomi2 <mmsomi2@jhb.dvt.co.za>
2024-06-19 12:06:40 +0200

    added exception calss for getting by category, not used currently

---
+++ shop-microservices-app/Services/Catalog/Catalog.API/Exceptions/ProductCategoryNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace Catalog.API.Exceptions
+{
+    public class ProductCategoryNotFoundException : Exception
+    {
+        public ProductCategoryNotFoundException() : base("Product category not found")
+        {
+        }
+    }
+}
