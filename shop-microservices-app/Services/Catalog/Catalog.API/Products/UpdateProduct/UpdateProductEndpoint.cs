e610adc587b583ceacadf4c0d7016da8f7932287
JHBDVT\mmsomi2 <mmsomi2@jhb.dvt.co.za>
2024-06-19 12:59:39 +0200

    added minimal api endpoint for updating a product

---
+++ shop-microservices-app/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductEndpoint.cs
@@ -0,0 +1,28 @@
+﻿using Carter;
+using Mapster;
+using MediatR;
+
+namespace Catalog.API.Products.UpdateProduct
+{
+    public record UpdateProductRequest(Guid Id, string Name, List<string> Category, string Description, string Image, decimal Price);
+    public record UpdateProductResponse(bool IsUpdated);
+    public class UpdateProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPut("/products", async (UpdateProductRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<UpdateProductCommand>();
+                var result = await sender.Send(command);
+                var response = result.Adapt<UpdateProductResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("UpdateProduct")
+            .WithDescription("Update a product")
+            .WithSummary("Update a product")
+            .Produces<UpdateProductResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound);
+        }
+    }
+}
