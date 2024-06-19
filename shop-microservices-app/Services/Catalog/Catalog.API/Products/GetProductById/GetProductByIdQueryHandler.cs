23fe755eb51ecb8408779f3d2067577f40770e7a
JHBDVT\mmsomi2 <mmsomi2@jhb.dvt.co.za>
2024-06-19 11:26:52 +0200

    added Handler class for getting product by Id

---
+++ shop-microservices-app/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Exceptions;
+using Catalog.API.Models;
+using Marten;
+
+namespace Catalog.API.Products.GetProductById
+{
+    public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+    public record GetProductByIdResult(Product Product);
+    internal class GetProductByIdQueryHandler(IDocumentSession session, ILogger<GetProductByIdQueryHandler> logger) : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+    {
+        public async Task<GetProductByIdResult> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Querying for product with {@request}", request);
+            var product = await session.LoadAsync<Product>(request.Id, cancellationToken);
+            if (product is null)
+            {
+                throw new ProductNotFoundException();
+            }
+            return new GetProductByIdResult(product);
+        }
+    }
+
+}
